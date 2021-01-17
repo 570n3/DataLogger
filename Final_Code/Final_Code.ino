9afbcc664868cd43a4185af6334be75aa8f35d67
MD. Minhazul Billah <45149268+570n3@users.noreply.github.com>
2021-01-17 19:52:04 +0600

    Add files via upload

---
+++ Final_Code/Final_Code.ino
@@ -0,0 +1,34 @@
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("DHT Final");
+  dht.begin();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(500);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature(true);
+
+  if(isnan(h) || isnan(t) || isnan(f)){
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+    }
+  float hif = dht.computeHeatIndex(f, h);
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.println(t);
+  Serial.println( h);
+
+}
